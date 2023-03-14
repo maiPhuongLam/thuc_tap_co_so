6d4d5ef547bae752caacc62a9666605974727431
Lam <maiphuonglambh.2002@gmail.com>
2023-03-15 04:02:32 +0700

    middlewares isAuth

--- server/src/middlewares/isAuth.ts
+++ server/src/middlewares/isAuth.ts
@@ -1,17 +1,19 @@
 import jwt from 'jsonwebtoken'
 import { Request, Response, NextFunction } from 'express';
 
+interface UserPayload {
+  userId: string
+}
+
 export default (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.get('Authorization');
     if (!authHeader) {
         return res.status(400).json('Not authorization')
     }
-
     const token = authHeader.split(' ')[1];
-    let decodedToken: any;
-
+    let decodedToken
     try {
-      decodedToken = jwt.verify(token, 'thuc_tap_co_so');
+        decodedToken = jwt.verify(token, 'thuc_tap_co_so') as UserPayload
     } catch (error) {
         let msg
         if (error instanceof Error) {
@@ -20,7 +22,7 @@ export default (req: Request, res: Response, next: NextFunction) => {
         return res.status(400).json({ status: 'fail', msg })
     }
     if (!decodedToken) {
-      return res.status(400).json({ status: 'fail', msg: 'Token is incorrect' })
+        return res.status(400).json({ status: 'fail', msg: 'Token is incorrect' })
     }
     req.userId = decodedToken.userId;
     next();
