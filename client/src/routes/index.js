167a758681f75e92fddac3c82a39eb158a8cdbca
Lam <maiphuonglambh.2002@gmail.com>
2023-03-18 02:04:03 +0700

    create-react-client

---
+++ client/src/routes/index.js
@@ -0,0 +1,24 @@
+import { HeaderOnly } from '../components/Layout';
+
+import Home from '../pages/Home';
+import Following from '../pages/Following';
+import Profile from '../pages/Profile';
+import Upload from '../pages/Upload';
+import Search from '../pages/Search';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+
+//Public routes
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+    { path: '/login', component: Login, layout: null },
+    { path: '/register', component: Register, layout: null },
+];
+
+const privateRoute = [];
+
+export { publicRoutes, privateRoute };
